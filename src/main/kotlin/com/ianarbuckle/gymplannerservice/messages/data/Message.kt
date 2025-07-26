339758f59554b0547d1d3e90e8afc8439eb8f0a0
ianarbuckle <ian.arbuckle18@gmail.com>
2025-07-26 23:01:29 +0100

    - Add timestamp validation

--- src/main/kotlin/com/ianarbuckle/gymplannerservice/messages/data/Message.kt
+++ src/main/kotlin/com/ianarbuckle/gymplannerservice/messages/data/Message.kt
@@ -1,15 +1,14 @@
 package com.ianarbuckle.gymplannerservice.messages.data
 
-import org.bson.codecs.pojo.annotations.BsonId
-import org.bson.types.ObjectId
 import jakarta.validation.constraints.Future
 import java.time.LocalDateTime
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
 
 data class Message(
     @BsonId val id: String = ObjectId.get().toHexString(),
     val userId: String,
     val username: String,
     val content: String,
-    @field:Future
-    val timestamp: LocalDateTime,
+    @field:Future val timestamp: LocalDateTime,
 )

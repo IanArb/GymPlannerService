960472dc092ba6ea011080eb72b189b810d2379d
ianarbuckle <ian.arbuckle18@gmail.com>
2026-04-19 22:09:54 +0100

    - Add CORS support

--- src/main/kotlin/com/ianarbuckle/gymplannerservice/authentication/data/security/SecurityConfig.kt
+++ src/main/kotlin/com/ianarbuckle/gymplannerservice/authentication/data/security/SecurityConfig.kt
@@ -3,6 +3,8 @@ package com.ianarbuckle.gymplannerservice.authentication.data.security
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
@@ -26,7 +28,9 @@ class SecurityConfig(
 ) {
     @Bean fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
-    @Bean fun corsWebFilter(): CorsWebFilter = CorsWebFilter(corsConfigurationSource())
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    fun corsWebFilter(): CorsWebFilter = CorsWebFilter(corsConfigurationSource())
 
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {

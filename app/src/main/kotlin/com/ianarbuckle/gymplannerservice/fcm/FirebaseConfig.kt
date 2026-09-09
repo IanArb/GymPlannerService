210e2c7cbb01adffd4be49417016817447ec0e8b
ianarbuckle <ian.arbuckle18@gmail.com>
2026-09-09 10:56:21 +0100

    [BW] Fix mongodb connection string changes

--- app/src/main/kotlin/com/ianarbuckle/gymplannerservice/fcm/FirebaseConfig.kt
+++ app/src/main/kotlin/com/ianarbuckle/gymplannerservice/fcm/FirebaseConfig.kt
@@ -4,9 +4,9 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
+import jakarta.annotation.PostConstruct
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import javax.annotation.PostConstruct
 
 @Configuration
 class FirebaseConfig {
